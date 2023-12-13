8d6862eae74c9277d527681d6320629bcdb5859f
Dwi Candra Kirana <dwicandra920@gmail.com>
2023-12-13 12:12:09 +0700

    fix on boarding screen

--- app/src/main/java/com/loc/newsapp/presentation/Dimension.kt
+++ app/src/main/java/com/loc/newsapp/presentation/Dimension.kt
@@ -7,4 +7,6 @@ object Dimension {
     val MediumPadding2 = 30.dp
 
     val IndicatorSize = 14.dp
+
+    val PageIndicatorWidth = 52.dp
 }
--- app/src/main/java/com/loc/newsapp/presentation/onboarding/OnBoardingScreen.kt
+++ app/src/main/java/com/loc/newsapp/presentation/onboarding/OnBoardingScreen.kt
@@ -1,20 +1,39 @@
 package com.loc.newsapp.presentation.onboarding
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.loc.newsapp.presentation.Dimension.MediumPadding2
+import com.loc.newsapp.presentation.Dimension.PageIndicatorWidth
+import com.loc.newsapp.presentation.common.NewsButton
+import com.loc.newsapp.presentation.common.NewsTextButton
 import com.loc.newsapp.presentation.onboarding.components.OnBoarding
+import com.loc.newsapp.presentation.onboarding.components.PageIndicator
+import com.loc.newsapp.ui.theme.NewsAppTheme
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun OnBoardingScreen() {
-    Column(modifier = Modifier.fillMaxSize()) {
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
         val pagerState = rememberPagerState(initialPage = 0) {
             pages.size
         }
@@ -34,5 +53,49 @@ fun OnBoardingScreen() {
         HorizontalPager(state = pagerState) { index ->
             OnBoarding(page = pages[index])
         }
+        Spacer(modifier = Modifier.weight(1f))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = MediumPadding2)
+                .navigationBarsPadding(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            PageIndicator(
+                modifier = Modifier.width(PageIndicatorWidth),
+                pageSize = pages.size,
+                selectedPage = pagerState.currentPage
+            )
+
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                val scope = rememberCoroutineScope()
+                if (buttonState.value[0].isNotEmpty()) {
+                    NewsTextButton(text = buttonState.value[0], onClick = {
+                        scope.launch {
+                            pagerState.animateScrollToPage(page = pagerState.currentPage - 1)
+                        }
+                    })
+                }
+                NewsButton(text = buttonState.value[1], onClick = {
+                    scope.launch {
+                        if (pagerState.currentPage == 3) {
+                            // TODO: Navigate to Home Screen
+                        } else {
+                            pagerState.animateScrollToPage(page = pagerState.currentPage + 1)
+                        }
+                    }
+                })
+            }
+        }
+        Spacer(modifier = Modifier.weight(0.5f))
+    }
+}
+
+@Preview
+@Composable
+fun OnBoardingPreview() {
+    NewsAppTheme {
+        OnBoardingScreen()
     }
 }
